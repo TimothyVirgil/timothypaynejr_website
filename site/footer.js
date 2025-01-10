6066013ec7cd90123303767c9393b84654b1de0a
Timothy Payne <timothypaynemath@gmail.com>
2025-01-10 15:04:29 -0500

    javascript footer

---
+++ site/footer.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = `
+      <footer>
+        <p>&#169  Timothy Payne Jr. 2025</p>
+      </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footer);
+  });
